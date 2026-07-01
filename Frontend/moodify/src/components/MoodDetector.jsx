e8ef6e29803fa9303110041f8e6c71f966b465d2
Keshav Singh Tanwar <keshavsinghtanwar39@gmail.com>
2026-07-01 16:32:01 +0530

    Added GitHub Pages deployment and backend Render configuration

--- Frontend/moodify/src/components/MoodDetector.jsx
+++ Frontend/moodify/src/components/MoodDetector.jsx
@@ -6,21 +6,20 @@ export default function MoodDetector({ setMood }) {
   const videoRef = useRef(null);
   const [currentMood, setCurrentMood] = useState("Click Button 🤔");
 
+  const MODEL_URL = `${import.meta.env.BASE_URL}models`;
+
   const detectMood = async () => {
     if (!videoRef.current) return;
 
     const result = await faceapi
-      .detectSingleFace(
-        videoRef.current,
-        new faceapi.TinyFaceDetectorOptions()
-      )
+      .detectSingleFace(videoRef.current, new faceapi.TinyFaceDetectorOptions())
       .withFaceExpressions();
 
     if (result) {
       const expressions = result.expressions;
 
       const detectedMood = Object.keys(expressions).reduce((a, b) =>
-        expressions[a] > expressions[b] ? a : b
+        expressions[a] > expressions[b] ? a : b,
       );
 
       setCurrentMood(detectedMood);
@@ -34,8 +33,8 @@ export default function MoodDetector({ setMood }) {
   useEffect(() => {
     const init = async () => {
       try {
-        await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-        await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+        await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+        await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
 
         const stream = await navigator.mediaDevices.getUserMedia({
           video: true,
@@ -56,19 +55,11 @@ export default function MoodDetector({ setMood }) {
   return (
     <div className="main-container">
       <div className="vid-container text-center text-2xl font-serif">
-        <video
-          ref={videoRef}
-          autoPlay
-          muted
-          playsInline
-        />
+        <video ref={videoRef} autoPlay muted playsInline />
 
         <h2>Current Mood : {currentMood}</h2>
 
-        <div
-          className="vid-btn"
-          onClick={detectMood}
-        >
+        <div className="vid-btn" onClick={detectMood}>
           Start Listening
         </div>
       </div>
